b2ea7837081d6f51888397a9c3990322d7cceb47
saifultamim38 <saifultamim38@gmail.com>
2025-08-21 14:28:47 +0600

    mentors2

--- app/mentors/page.jsx
+++ app/mentors/page.jsx
@@ -4,6 +4,7 @@ function MentorsPage() {
   return (
     <div>
          <button className="bg-gray-300 px-6 py-3 font-bold rounded-lg">mentors</button>
+         <button className="bg-gray-300 px-6 py-3 font-bold rounded-lg mt-7">mentors2</button>
     </div>
   )
 }
