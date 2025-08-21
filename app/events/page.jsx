6cdc00d5f11376d50883e519d2d4c5fea2804ff9
saifultamim38 <saifultamim38@gmail.com>
2025-08-21 15:24:31 +0600

    events

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function EventsPage() {
   return (
     <div>
-         <button className="bg-gray-300 px-6 py-3 font-bold rounded-lg ">events2</button>
+         <button className="bg-gray-300 px-6 py-3 font-bold rounded-lg ">events22</button>
     </div>
   )
 }
