bc36f8226cb2bf92cf52bf0a6961a19cda71c954
saifultamim38 <saifultamim38@gmail.com>
2025-08-21 14:25:29 +0600

    about2

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function AboutPage() {
   return (
     <div>
-         <button className="bg-gray-300 px-6 py-3 font-bold rounded-lg">about</button>
+         <button className="bg-gray-300 px-6 py-3 font-bold rounded-lg">about</button> <br/>
+         <button className="bg-gray-300 px-6 py-3 font-bold rounded-lg mt-7">about2</button>
     </div>
   )
 }
