f5ff406546a0a8e70a7abb684766b7850cacb947
saifultamim38 <saifultamim38@gmail.com>
2025-08-21 16:44:54 +0600

    changes

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -7,7 +7,7 @@ function AboutPage() {
 
          <button className="bg-gray-300 px-6 py-3 font-bold rounded-lg mt-7">about23</button>
          <h2>about branch update</h2>
-         <h2>events banch about page </h2>
+         <h2>events banch about page 3</h2>
       
     </div>
   )
