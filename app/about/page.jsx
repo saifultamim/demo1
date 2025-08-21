62edeb1446cb424ff87960506e8bb9a0d6583c91
saifultamim38 <saifultamim38@gmail.com>
2025-08-21 16:31:03 +0600

    about branch

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -5,6 +5,7 @@ function AboutPage() {
     <div>
          <button className="bg-gray-300 px-6 py-3 font-bold rounded-lg">about</button> <br/>
          <button className="bg-gray-300 px-6 py-3 font-bold rounded-lg mt-7">about23</button>
+         <h2>about branch </h2>
     </div>
   )
 }
