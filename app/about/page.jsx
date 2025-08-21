3a20e08c3eec6cc48a3d96ca2b5dc620451eb264
saifultamim38 <saifultamim38@gmail.com>
2025-08-21 14:00:24 +0600

    about

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AboutPage() {
+  return (
+    <div>
+         <button className="bg-gray-300 px-6 py-3 font-bold rounded-lg">about</button>
+    </div>
+  )
+}
+
+export default AboutPage
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
   <div>
-    <p>demo </p>
+    <button className="bg-gray-300 px-6 py-3 font-bold rounded-lg">demo1</button>
   </div>
   );
 }
