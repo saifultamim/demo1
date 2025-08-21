6637d686b7431f794af0767aec9872a511087151
saifultamim38 <saifultamim38@gmail.com>
2025-08-21 15:38:56 +0600

    events

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -5,6 +5,7 @@ function AboutPage() {
     <div>
          <button className="bg-gray-300 px-6 py-3 font-bold rounded-lg">about</button> <br/>
          <button className="bg-gray-300 px-6 py-3 font-bold rounded-lg mt-7 ">about23</button>
+            <button className="bg-gray-300 px-6 py-3 font-bold rounded-lg mt-7 ">about23</button>
     </div>
   )
 }
