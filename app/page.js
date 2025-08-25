cdf49ac75baafdda330800f7bfd3672fa621da38
saifultamim38 <saifultamim38@gmail.com>
2025-08-25 11:36:09 +0600

    events

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-  <div>
-    <button className="bg-gray-300 px-6 py-3 font-bold rounded-lg">demo1</button>
-  </div>
+    <div>
+      <button className="bg-gray-300 px-6 py-3 font-bold rounded-lg">
+        demo1
+      </button>
+    </div>
   );
 }
